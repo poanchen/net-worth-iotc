7ad495724b35fc3246dacafc0986ff7f052ab1c9
poanchen <chen.baron@hotmail.com>
2021-04-16 03:32:07 -0700

    typo

--- configs/poloniex_config.py
+++ configs/poloniex_config.py
@@ -1,7 +1,7 @@
 poloniex_config = {}
 
 # domain
-poloniex_config['api_domain'] = 'azure_functions_end_point'
+poloniex_config['api_domain'] = 'https://poloniex.com'
 
 # api key
 poloniex_config['api_public_key'] = 'kraken_api_public_key_name'
